d93d5b8ca9f6407f7f102fd4a9fd2022676d1be8
Sergei Nabatov <serynabatov@yandex.ru>
2025-05-31 19:59:06 +0200

    for review

--- frontend/src/features/auth/repository.ts
+++ frontend/src/features/auth/repository.ts
@@ -1,16 +1,17 @@
-/**
- * 
- * const { createQuery } = something("user"):
- * 
- * const authConstroller = controller;
- * 
- * const userRepository = {
- *  login: (params: Authorization) => createQuery(() => axiosExtract(authConstroller.login(params)))
- * }
- * 
- * 
- * 
- * userRepository.login.useQuery(params)
- * userRepository.login.fech(params)
- * 
- */
+import { axiosExtract } from "@/lib/async";
+import type { Authentication } from "./form";
+import { axiosClient } from "@/api";
+
+const {createQuery} = null;
+
+const authConstroller = {
+  login: (params: Authentication) => axiosClient.post('/api/login', params),
+  register: (params: Authentication) => axiosClient.post('/api/register', params),
+  googleSignIn: () => axiosClient.get('api/google/login')
+}
+
+const authRepository = {
+  login: (params: Authentication) => createQuery(() => axiosExtract(authConstroller.login(params))),
+  register: (params: Authentication) => createQuery(() => axiosExtract(authConstroller.register(params))),
+  googleSignIn: () => createQuery(() => axiosExtract(authConstroller.googleSignIn()))
+}
