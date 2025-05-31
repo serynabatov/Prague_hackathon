6523871ac2d60e6f0193a20b8cd8dcc9b7af47b2
Sergei Nabatov <serynabatov@yandex.ru>
2025-05-31 20:36:53 +0200

    interceptor

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -13,4 +13,38 @@ const axiosConfig = {
 
 const axiosClient = Axios.create(axiosConfig);
 
+function getToken(): string | null {
+  return localStorage.getItem("token");
+}
+
+axiosClient.interceptors.request.use(
+  (config: any) => {
+    const token = getToken();
+    if (token) {
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${token}`
+      };
+    }
+
+    return config;
+  },
+  (error: any) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosClient.interceptors.request.use(
+  (response: any) => {
+    return response;
+  },
+  (error: any) => {
+    if ((error.response?.status == 401) || (error.response?.status == 403)) {
+      localStorage.removeItem("token");
+    }
+
+    return Promise.reject(error);
+  }
+)
+
 export { axiosClient };
